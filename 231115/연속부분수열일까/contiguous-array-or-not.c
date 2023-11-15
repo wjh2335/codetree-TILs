ac6d9be21d3b22ccba73855c766460b647f15cf6
wjh2335 <38124814+wjh2335@users.noreply.github.com>
2023-11-15 11:08:31 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231115/연속부분수열일까/contiguous-array-or-not.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n1, n2, arr1[101], arr2[101], flag;
+    scanf("%d %d", &n1, &n2);
+    for(int i = 0; i < n1; ++i) scanf("%d", &arr1[i]);
+    for(int i = 0; i < n2; ++i) scanf("%d", &arr2[i]);
+    for(int i = 0; i < n1 - n2; ++i)
+    {
+        flag = 1;
+        for(int j = 0; j < n2; ++j)
+        {
+            if(arr1[i + j] != arr2[i + j]) flag = 0;
+        }
+        if(flag) { printf("Yes"); break; }
+    }
+    if(!flag) printf("No");
+    return 0;
+}
