8d28a2e1b7636a68d7f560d7fa7935fcf67d1a1e
wjh2335 <38124814+wjh2335@users.noreply.github.com>
2023-11-15 10:29:25 +0900

    Title: 제곱하여 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231115/제곱하여 출력하기/print-square-of-elements.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        int a;
+        scanf("%d", &a);
+        printf("%d ", a * a);
+    }
+    return 0;
+}
